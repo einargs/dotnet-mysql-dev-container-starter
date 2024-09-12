edabbb6f26c5dab3c63948bd4d9541e0e32e3c5e
Einar Strandberg <einargs@gmail.com>
2024-09-12 17:37:45 -0500

    Before migrations added

---
+++ Model.cs
@@ -0,0 +1,34 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using MySql.EntityFrameworkCore.Extensions;
+using MySql.EntityFrameworkCore;
+using System.Collections.Generic;
+
+public class BloggingContext : DbContext
+{
+    public DbSet<Blog> Blogs { get; set; }
+    public DbSet<Post> Posts { get; set; }
+
+    // The following configures EF to create a Sqlite database file in the
+    // special "local" folder for your platform.
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+        => options.UseMySQL("Server=db; User ID=root; Password=password; Database=blog");
+}
+
+public class Blog
+{
+    public int BlogId { get; set; }
+    public string Url { get; set; }
+
+    public List<Post> Posts { get; } = new();
+}
+
+public class Post
+{
+    public int PostId { get; set; }
+    public string Title { get; set; }
+    public string Content { get; set; }
+
+    public int BlogId { get; set; }
+    public Blog Blog { get; set; }
+}
